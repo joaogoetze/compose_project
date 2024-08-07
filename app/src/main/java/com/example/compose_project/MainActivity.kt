0dce47d6bdf7f4032a34a876b8355b7c0a6efce6
joaogoetze <joaoaugustoferreira5@gmail.com>
2024-08-06 22:30:22 -0300

    Adding TextFields

--- app/src/main/java/com/example/compose_project/MainActivity.kt
+++ app/src/main/java/com/example/compose_project/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.compose_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,7 +36,32 @@ class MainActivity : ComponentActivity() {
 }
 
 //Meu componente visual "APLICATIVO"
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(){
 
+    Column{
+        Text(text = "Gasolina ou Alcool")
+        Text(text = "Gasolina")
+        TextField(value = "2.00", onValueChange = {
+
+        })
+        TextField(value = "2.00", onValueChange = {
+
+        })
+    }
+
+    //Mantem os elementos um em cima do outro
+    //Box{
+        //Text(text = "Alcool ou Gasolina")
+        //Text(text = "Gasolina")
+    //}
+
+    //Mantem os iconer um abaixo do outro
+
+    //Mantem os elemntos um ao lado do outro
+    //Row{
+        //Text(text = "Alcool ou Gasolina")
+        //Text(text = "Gasolina")
+    //}
 }
